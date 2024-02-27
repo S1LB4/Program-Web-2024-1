e1bfa25cd0f9daf4de8788927f457dcf0df53188
S1LB4 <113909639+S1LB4@users.noreply.github.com>
2024-02-26 23:25:56 -0500

    Delete WEB directory

--- WEB/main.jsx
+++
@@ -1,8 +0,0 @@
-import './style.css'
-import {createRoot } from "react-dom/client"
-import App from './App'
-const appContainer = document.querySelector("#app")
-const root = createRoot(appContainer)
-
-root.render(<App />) ;
-console.log("Hello World")
