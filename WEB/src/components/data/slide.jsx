9dfdd673d8efded97dca567121bdee8d3f56eddc
S1LB4 <113909639+S1LB4@users.noreply.github.com>
2024-04-03 21:53:22 -0500

    Delete WEB directory

--- WEB/src/components/data/slide.jsx
+++
@@ -1,8 +0,0 @@
-export const slider = [
-    {img: "https://m.media-amazon.com/images/I/81KkrQWEHIL._SX3000_.jpg" },
-    {img: "https://m.media-amazon.com/images/I/61Pdr3h6MmL._SX3000_.jpg" },
-    {img: "https://m.media-amazon.com/images/I/61zAjw4bqPL._SX3000_.jpg" },
-    {img: "https://m.media-amazon.com/images/I/71EinHErCtL._SX3000_.jpg" },
-    {img: "https://m.media-amazon.com/images/I/61CiqVTRBEL._SX3000_.jpg"},
-]
-
